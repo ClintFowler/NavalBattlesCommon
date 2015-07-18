ca401dfd9d5aaff2912c33b7f3e4d9e7f3a99929
ClintFowler <jedd1876@gmail.com>
2015-07-18 00:59:41 -0600

    Initial structure for ship class

--- src/Common/Ship.java
+++ src/Common/Ship.java
@@ -5,5 +5,60 @@ package Common;
  */
 public class Ship
 {
+    private int size;
+    private String direction;
+    private int xcoordinate;
+    private int ycoordinate;
+    private int hits;
 
+    public Ship(int ssize, String facing, int x, int y)
+    {
+        size = ssize;
+        direction = facing;
+        xcoordinate = x;
+        ycoordinate = y;
+        hits = 0;
+    }
+
+    //Accessor Methods
+    protected int getXcoordinate()
+    {
+        int x = xcoordinate;
+        return x;
+    }
+
+    protected int getYcoordinate()
+    {
+        int y = ycoordinate;
+        return y;
+    }
+
+    protected int getSize()
+    {
+        int s = size;
+        return s;
+    }
+
+    protected String getDirection()
+    {
+        String d = direction;
+        return d;
+    }
+
+    protected void addHit()
+    {
+        if(hits < size)
+        {
+            hits++;
+        }
+    }
+
+    protected boolean hasBeenSunk()
+    {
+        if (hits == size)
+        {
+            return true;
+        }
+        return false;
+    }
 }
