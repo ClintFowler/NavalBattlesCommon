d13259f800516ff1db74fa5bfff308d91d361b30
ClintFowler <jedd1876@gmail.com>
2015-07-19 02:27:10 -0600

    Added method to start thread for server connection

--- src/Common/GameFramework.java
+++ src/Common/GameFramework.java
@@ -5,9 +5,21 @@ package Common;
  */
 public class GameFramework
 {
+    //game constants
+    public static final int CARRIER = 5;
+    public static final int BATTLESHIP = 4;
+    public static final int SUBMARINE = 3;
+    public static final int CRUISER = 3;
+    public static final int DESTROYER = 2;
+    public static final String NORTH = "NORTH";
+    public static final String SOUTH = "SOUTH";
+    public static final String EAST = "EAST";
+    public static final String WEST = "WEST";
+
+    // basic structure for game
     private ServerConnection server;
     private String userid = "Clint";
-    //add your game client window here
+    //add your game client window class here
 
     public GameFramework()
     {
@@ -19,6 +31,13 @@ public class GameFramework
     protected void messageHandler()
     {
         //TODO: Message Logic once Message class is built by Trevor
+    }
+
+    //start a thread for server connection
+    protected void startServer()
+    {
+        Thread thread = new Thread(server);
+        thread.start();
     }
 
     /**
